20ea417accd943bc208b27b6a6fd3a0fd60ab176
Luiz Gomes <lgomes_kiau@hotmail.com>
2022-04-16 09:41:50 -0300

    sum_of_prices_lambda

---
+++ course/src/br/com/luiz/exemple1/service/ProductService.java
@@ -0,0 +1,20 @@
+package br.com.luiz.exemple1.service;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import br.com.luiz.exemple1.entitie.Product;
+
+public class ProductService {
+	
+	public double filteredSum(List<Product> list, Predicate<Product> criteria) {
+		double sum = 0.0;
+		for (Product p : list) {
+			if (criteria.test(p)) {
+				sum += p.getPrice();
+			}
+		}
+		return sum;
+	}
+
+}
